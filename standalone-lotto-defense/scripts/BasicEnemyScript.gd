2e7c858bf9d0677559648194b723163658f60fe7
Sacredng <kraveblaze@gmail.com>
2026-05-19 06:11:28 +0100

    Enemy Pathing

--- standalone-lotto-defense/scripts/BasicEnemyScript.gd
+++ standalone-lotto-defense/scripts/BasicEnemyScript.gd
@@ -1,6 +1,21 @@
 extends CharacterBody2D
 
-@export var speed = 100
+@export var max_health = 10
 
-func _physics_process(delta):
-	position.x += speed * delta
+var current_health
+
+func _ready():
+	current_health = max_health
+
+func take_damage(amount):
+	current_health -= amount
+
+	if current_health <= 0:
+		die()
+
+func die():
+	queue_free()
+	
+func _input(event):
+	if event.is_action_pressed("ui_accept"):
+		take_damage(5)
